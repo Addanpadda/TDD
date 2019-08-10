30d405951dc36b14d44e4fde8fc695cd5a1a39ba
Addanpadda <adamherbertsson@gmail.com>
2019-08-10 21:06:32 +0200

    Commit

--- GameOfLife/tester.py
+++ GameOfLife/tester.py
@@ -7,10 +7,13 @@ class Cell():
         return True
 
 class TestStringMethods(unittest.TestCase):
-    
-    def test_upper(self):
+    def test_CheckAlive_True(self):
         cell = Cell()
-        self.assertTrue(cell.CheckAlive(), True)
+        self.assertTrue(cell.CheckAlive())
+
+    def test_CheckAlive_False(self):
+        cell = Cell()
+        self.assertFalse(cell.CheckAlive())
 
 if __name__ == '__main__':
     unittest.main()
