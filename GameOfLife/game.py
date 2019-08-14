1be1b9284bdd4b68f817d526b4c93af949a5bb12
Addanpadda <adamherbertsson@gmail.com>
2019-08-14 16:41:47 +0200

    Commit

--- GameOfLife/game.py
+++ GameOfLife/game.py
@@ -1,3 +1,55 @@
 #!/bin/python3.7
-def CheckAlive(cell):
 
+class Cell():
+    _isAlive = 0
+    
+    def Alive(self, isAlive):
+        self._isAlive = isAlive
+
+    def CheckAlive(self):
+        return self._isAlive
+
+class Universe():
+    _dimentions = [0, 0]
+    space = list()
+
+    def __init__(self, dimention_x, dimention_y):
+        self._dimentions = [dimention_x, dimention_y]
+        self.space = self.CreateUniverse(dimention_x, dimention_y)
+
+    def CreateUniverse(self, x_dimention, y_dimention):
+        cells = []
+        
+        for y in range(y_dimention):
+            cells.append([])
+            for x in range(x_dimention):
+                cells[y].append(Cell())
+        
+        return cells
+
+    def Neighbours(self, x, y):
+        neighbours = 0
+        neighbourTest = [x+1, y+1]
+
+        while True:
+            for x in range(2):
+                if int(neighbourTest[0]) >= 0 and int(neighbourTest[1]) >= 0:
+                    neighbours += self.space[neighbourTest[0]][neighbourTest[1]].CheckAlive()
+                neighbourTest[0] -= 1
+            
+            for x in range(2):
+                if neighbourTest[0] >= 0 and neighbourTest[1] >= 0:
+                    neighbours += self.space[neighbourTest[0]][neighbourTest[1]].CheckAlive()
+                neighbourTest[1] -= 1
+
+            for x in range(2):                                                           
+                if neighbourTest[0] >= 0 and neighbourTest[1] >= 0:
+                    neighbours += self.space[neighbourTest[0]][neighbourTest[1]].CheckAlive()
+                neighbourTest[0] += 1
+
+            for x in range(2):
+                if neighbourTest[0] >= 0 and neighbourTest[1] >= 0:
+                    neighbours += self.space[neighbourTest[0]][neighbourTest[1]].CheckAlive()
+                neighbourTest[1] += 1
+            
+            return neighbours
--- GameOfLife/tester.py
+++ GameOfLife/tester.py
@@ -1,19 +1,60 @@
 #!/bin/python3.7
-
 import unittest
+from game import *
 
-class Cell():
-    def CheckAlive(self):
-        return True
-
-class TestStringMethods(unittest.TestCase):
-    def test_CheckAlive_True(self):
+class Tests(unittest.TestCase):
+    def test_Cell_CheckAlive_True(self):
         cell = Cell()
+        cell.Alive(True)
         self.assertTrue(cell.CheckAlive())
 
-    def test_CheckAlive_False(self):
+    def test_Cell_CheckAlive_False(self):
         cell = Cell()
+        cell.Alive(False)
         self.assertFalse(cell.CheckAlive())
+    
+    def test_Universe_CreateUniverse(self):
+        universe = Universe(3, 3)
+        self.assertEqual(sum(len(x) for x in universe.space), 9)
+
+    def test_Universe_Neighbours_diagonal(self):
+        universe = Universe(3, 3)
+
+        universe.space[0][0].Alive(True)
+        self.assertEqual(universe.Neighbours(1, 1), 1)
+        universe.space[0][2].Alive(True)
+        self.assertEqual(universe.Neighbours(1, 1), 2)
+        universe.space[2][0].Alive(True)
+        self.assertEqual(universe.Neighbours(1, 1), 3)
+        universe.space[2][2].Alive(True)
+        self.assertEqual(universe.Neighbours(1, 1), 4)
+
+    def test_Universe_Neighbours_diagonal_edge_of_space(self):
+        universe = Universe(3, 3)
+
+        universe.space[1][1].Alive(True)
+        self.assertEqual(universe.Neighbours(0, 0), 1)
+    
+    def test_Universe_Neighbours_linear(self):
+        universe = Universe(3, 3)
+
+        universe.space[0][1].Alive(True)
+        self.assertEqual(universe.Neighbours(1, 1), 1)
+        universe.space[1][0].Alive(True)
+        self.assertEqual(universe.Neighbours(1, 1), 2)
+        universe.space[1][2].Alive(True)
+        self.assertEqual(universe.Neighbours(1, 1), 3)
+        universe.space[2][1].Alive(True)
+        self.assertEqual(universe.Neighbours(1, 1), 4)
+
+    def test_Universe_Neighbours_linear_edge_of_space(self):
+        universe = Universe(3, 3)
+
+        universe.space[0][1].Alive(True)
+        self.assertEqual(universe.Neighbours(0, 0), 1)
+        universe.space[1][0].Alive(True)
+        self.assertEqual(universe.Neighbours(0, 0), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
